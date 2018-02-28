3655a670b54ad5e77cfb3fbb6ee733d27c770a38
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-02-27 17:50:18 -0800

    fixed routes

--- finalProject.py
+++ finalProject.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 #Initial
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>')
-def showRestaurants():
+def showRestaurants(restaurant_id):
     restaurant_id = "This page will show all my restaurants"
     return restaurant_id
 
 #Create a new restaurant
 @app.route('/restaurant/new')
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
-def newRestaurant():
+def newRestaurant(restaurant_id):
     restaurant_id = "This page will be for making new restaurants"
     return restaurant_id
 
@@ -19,39 +19,38 @@ def newRestaurant():
 #Edit a restaurant
 @app.route('/restaurant/<int:restaurant_id>/edit')
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
-def editRestaurant():
-    restaurant_id = "restaurant_id"
-    menu_id = "menu_id"
-    return restaurant_id
+def editRestaurant(restaurant_id,menu_id):
+    return "edit restaurant's menu"
 
 #Delete a restaurant
 @app.route('/restaurant/<int:restaurant_id>/delete')
-def deleteRestaurant():
+def deleteRestaurant(restaurant_id):
     restaurant_id = "This page will be for deleting restaurant"
     return restaurant_id
 
 #Show menu
-@app.route('/restaurant/restaurant_id')
-@app.route('/restaurant/restaurant_id/menu')
-def showMenu():
+@app.route('/restaurant/<int:restaurant_id>')
+@app.route('/restaurant/<int:restaurant_id>/menu')
+def showMenu(restaurant_id):
     restaurant_id = "This page is for making a new menu for a restaurant"
     return restaurant_id
 
 #New Menu Item
-@app.route('/restaurant_id/menu/new')
-def newMenuItem():
-    return "This page is for making new menu"
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
+def newMenuItem(restaurant_id):
+    restaurant_id = "This page is for making a new menu"
+    return restaurant_id
 
 #Edit Menu Item
-@app.route('/restaurant/restaurant_id/menu/menu_id/edit/')
-def editMenuItem():
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id,menu_id):
     return "This page is to edit menu"
 
-#Delete Menu Item
-@app.route('/restaurant_id/menu/menu_id/delete')
-def deleteMenuItem():
-    return "This page deletes the menu"
 
+#Delete Menu Item
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id,menu_id):
+    return "This page is to delete the menu"
 
 
 if __name__ == '__main__':
